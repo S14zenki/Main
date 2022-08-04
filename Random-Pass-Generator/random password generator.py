7827f43f46f8202877284f9916b51c53023f56af
William Shannon <williamshannon@outlook.com>
2022-08-04 10:52:53 +0800

    added some changes to generate random password.py

--- Random-Pass-Generator/random password generator.py
+++ Random-Pass-Generator/random password generator.py
@@ -7,25 +7,22 @@ characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 def generate_random_password():
 	## length of password from the user
-	length = input("Enter Length Of Password")
-        if length > 0 and length < 50
-	    ## shuffling the characters
-	    random.shuffle(characters)
-	
-	    ## picking random characters from the list
-	    password = []
-	    for i in range(length):
+	length = 0
+	while length <= 0 or length > 50:
+		length = int(input("Enter Length Of Password: "))
+	## shuffling the characters
+	random.shuffle(characters)
+	## picking random characters from the list
+	password = []
+	for i in range(length):
 		password.append(random.choice(characters))
+	## shuffling the resultant password
+	random.shuffle(password)
+	## converting the list to string
+	## printing the list
+	print("".join(password))
+	print(len(password))
 
-	    ## shuffling the resultant password
-	    random.shuffle(password)
-
-	    ## converting the list to string
-	    ## printing the list
-	    print("".join(password))
-            print(len(password))
-        else:
-            length = input("Enter Length Of Password")
 
 
 ## invoking the function
